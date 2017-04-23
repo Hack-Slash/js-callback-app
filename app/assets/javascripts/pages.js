56450871b469b9aa8c0fabf9b6e43f7ac794728d
Brian <riceb53@gmail.com>
2017-04-23 09:31:11 -0400

    Add async API call

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,30 +1,42 @@
-function changeColors() {
-  var firstOne = document.getElementById('first');
-  firstOne.style.color = 'red';
-  setTimeout(function() {
-    var secondOne = document.getElementById('second');
-    secondOne.style.color = 'blue';
-    setTimeout(function() {
-      var thirdOne = document.getElementById('third');
-      thirdOne.style.color = 'green';
-      setTimeout(function() {
-        var fourthOne = document.getElementById('fourth');
-        fourthOne.style.color = 'orange';
+// function changeColors() {
+//   var firstOne = document.getElementById('first');
+//   firstOne.style.color = 'red';
+//   setTimeout(function() {
+//     var secondOne = document.getElementById('second');
+//     secondOne.style.color = 'blue';
+//     setTimeout(function() {
+//       var thirdOne = document.getElementById('third');
+//       thirdOne.style.color = 'green';
+//       setTimeout(function() {
+//         var fourthOne = document.getElementById('fourth');
+//         fourthOne.style.color = 'orange';
 
-      }, 2000)
-    }, 2000)
-  }, 2000);
+//       }, 2000)
+//     }, 2000)
+//   }, 2000);
 
-  setInterval(function() {
-    document.querySelector('body').style['background-color'] = getRandomColor();
-  }, 1000);
-}
+//   setInterval(function() {
+//     document.querySelector('body').style['background-color'] = getRandomColor();
+//   }, 1000);
+// }
 
-    function getRandomColor() {
-     var letters = '0123456789ABCDEF';
-     var color = '#';
-     for (var i = 0; i < 6; i++ ) {
-       color += letters[Math.floor(Math.random() * 16)];
-     }
-     return color;
-   }
+//     function getRandomColor() {
+//      var letters = '0123456789ABCDEF';
+//      var color = '#';
+//      for (var i = 0; i < 6; i++ ) {
+//        color += letters[Math.floor(Math.random() * 16)];
+//      }
+//      return color;
+//    }
+
+$.get("https://data.cityofnewyork.us/resource/cdpt-29ur.json").done(
+  function(response) {
+    console.log(response);
+  }
+);
+
+
+
+$.get("https://data.cityofnewyork.us/resource/cdpt-29ur.json", function(response) {
+  console.log(response);
+});
