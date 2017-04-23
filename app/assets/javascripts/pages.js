e393908117f49611bca74f7c73bdf21df7e86863
Brian <riceb53@gmail.com>
2017-04-23 11:35:50 -0400

    Toggle info and toggle color

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,3 +1,4 @@
+/* global $ */
 // function changeColors() {
 //   var firstOne = document.getElementById('first');
 //   firstOne.style.color = 'red';
@@ -10,7 +11,6 @@
 //       setTimeout(function() {
 //         var fourthOne = document.getElementById('fourth');
 //         fourthOne.style.color = 'orange';
-
 //       }, 2000)
 //     }, 2000)
 //   }, 2000);
@@ -28,15 +28,48 @@
 //      }
 //      return color;
 //    }
+function getData() {
+  var divEmployees = document.getElementById('markets');
+  divEmployees.innerHTML = 'loading...';
+  $.get("https://data.cityofnewyork.us/resource/cdpt-29ur.json").done(
+    function(response) {
+      // console.log(response);
+      var contentString = '';
+      for (var i = 0; i < response.length; i++) {
+        contentString += "<div onclick='changeMarket(this)'>";
+        contentString += '<h5 onclick="hideData(this)">' + response[i].market_name + '</h5>';
+        contentString += '<div class="info">';
+        contentString += '<p>' + response[i].borough + '</p>';
+        contentString += '<p>' + response[i].day_s + '</p>';
+        contentString += '<p>' + response[i].hours + '</p>';
+        contentString += '</div>';
+        contentString += '</div>';
+        contentString += '<hr>';
+      }
+      divEmployees.innerHTML = contentString;
+    }
+  );
+}
 
-$.get("https://data.cityofnewyork.us/resource/cdpt-29ur.json").done(
-  function(response) {
-    console.log(response);
+function changeMarket(theMarket) {
+  console.log('I changed the market');
+
+  if (theMarket.style.color === 'lightgray') {
+    theMarket.style.color = 'black';
+  } else {
+    theMarket.style.color = 'lightgray';
   }
-);
+}
 
+function hideData(theHeader) {
 
+  // theHeader.parentElement.children[1].style.display = 'none';
+  console.log('hiding the data');
 
-$.get("https://data.cityofnewyork.us/resource/cdpt-29ur.json", function(response) {
-  console.log(response);
-});
+  if (theHeader.parentElement.children[1].style.display === 'none') {
+    theHeader.parentElement.children[1].style.display = 'block';
+  } else {
+    theHeader.parentElement.children[1].style.display = 'none';
+  }
+}
+
